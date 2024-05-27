5f91b4d822db9848aa9f62c92d7ac010c9cea192
Ivica Čardić <ivica.cardic@gmail.com>
2024-05-27 20:51:29 +0200

    - client - checkProjectWorkflowStatus

--- client/src/pages/automation/project/Project.tsx
+++ client/src/pages/automation/project/Project.tsx
@@ -183,11 +183,11 @@ const Project = () => {
     const updateWorkflowMutation = useUpdatePlatformWorkflowMutation({
         onSuccess: () => {
             queryClient.invalidateQueries({
-                queryKey: ProjectKeys.project(parseInt(projectId!)),
+                queryKey: ProjectWorkflowKeys.projectWorkflow(parseInt(projectId!), parseInt(projectWorkflowId!)),
             });
 
             queryClient.invalidateQueries({
-                queryKey: ProjectWorkflowKeys.projectWorkflow(parseInt(projectId!), parseInt(projectWorkflowId!)),
+                queryKey: ProjectWorkflowKeys.projectWorkflows(parseInt(projectId!)),
             });
 
             setShowEditWorkflowDialog(false);
@@ -200,11 +200,7 @@ const Project = () => {
     const updateWorkflowNodeParameterMutation = useUpdateWorkflowNodeParameterMutation({
         onSuccess: () => {
             queryClient.invalidateQueries({
-                queryKey: ProjectKeys.project(parseInt(projectId!)),
-            });
-
-            queryClient.invalidateQueries({
-                queryKey: WorkflowKeys.workflow(workflow.id!),
+                queryKey: ProjectWorkflowKeys.projectWorkflow(parseInt(projectId!), parseInt(projectWorkflowId!)),
             });
         },
     });
